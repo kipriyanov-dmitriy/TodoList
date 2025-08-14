4178606cff6ef205d973e214d0cccb48a63c9475
Kipriyanov Dmitriy <kassim2111@gmail.com>
2025-08-14 18:00:27 +0500

    added screen

--- app/src/main/java/com/example/todo/ui/host/TodoListMainActivity.kt
+++ app/src/main/java/com/example/todo/ui/host/TodoListMainActivity.kt
@@ -4,22 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.todo.theme.TODOTheme
+import com.example.todo.ui.theme.TodoTheme
+import com.example.todo.ui.todo_list_screen.TodoListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TODOTheme {
-
+            TodoTheme {
+                TodoListScreen()
             }
         }
     }
--- app/src/main/java/com/example/todo/ui/todo_list_screen/TodoListScreen.kt
+++ app/src/main/java/com/example/todo/ui/todo_list_screen/TodoListScreen.kt
@@ -1,13 +1,23 @@
 package com.example.todo.ui.todo_list_screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.todo.core.PreviewPhone
-import com.example.todo.ui.theme.TODOTheme
+import com.example.todo.ui.components.ListItem
+import com.example.todo.ui.theme.TodoTheme
 import org.koin.compose.viewmodel.koinViewModel
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun TodoListScreen(
@@ -15,13 +25,33 @@ fun TodoListScreen(
 ) {
     val viewModel: TodoListViewModel = koinViewModel()
 
+    TodoListContent()
+}
+
+@Composable
+private fun TodoListContent(
+
+){
+    val list = remember {
+        List(35) { index -> "Hello world #$index" }
+    }
     Scaffold(
 
     ) { paddingValues ->
         LazyColumn(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(4.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-
+            items(list) { item ->
+                ListItem(
+                    text = item
+                ) {
+                    println("Clicked: $item")
+                }
+            }
         }
     }
 }
@@ -29,7 +59,7 @@ fun TodoListScreen(
 @Composable
 @PreviewPhone
 private fun TodoListPreview() {
-    TODOTheme {
-        TodoListScreen()
+    TodoTheme {
+        TodoListContent()
     }
 }
