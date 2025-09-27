d13ce938814b5711358a54c7d989a40d1ca7eea5
GitHub Action <action@github.com>
2025-09-27 10:30:39 +0000

    Bump version to ..1 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.example.todo"
         minSdk = 28
         targetSdk = 36
-        versionCode = 16
+        versionCode = 17
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
